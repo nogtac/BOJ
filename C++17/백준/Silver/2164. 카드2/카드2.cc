5586338dd8bdd348066f71f11866cbe046c4190b
nogtac <72373048+nogtac@users.noreply.github.com>
2026-04-13 18:39:49 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {  
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n;
+    queue<int> q;
+
+    for(int i = 1; i <= n; i++) q.push(i);
+
+    while(q.size() != 1) {
+        q.pop();
+
+        if(q.size()!=1) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    cout << q.front();
+    return 0;
+}
